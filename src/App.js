7b4a714d39a4fcdc5d57ad510ec3cd8246fd7809
tincurar <tincurares13@gmail.com>
2022-12-02 17:36:24 +0100

    need to create navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/4-useState-object'
+import Chapter1 from './tutorial/1-useState/setup/1-error-example'
+import Chapter2 from './tutorial/1-useState/setup/2-useState-basics'
+import Chapter3 from './tutorial/1-useState/setup/3-useState-array'
+import Chapter4 from './tutorial/1-useState/setup/4-useState-object'
+import Chapter5 from './tutorial/1-useState/setup/5-useState-counter'
 
 function App() {
   return (
     <div className='container'>
       <h2>Advanced Tutorial</h2>
-      <Setup />
+      {/* TODO: add navbar with react router */}
+      <Chapter1 />
+      <Chapter2 />
+      <Chapter3 />
+      <Chapter4 />
+      <Chapter5 />
     </div>
   )
 }
