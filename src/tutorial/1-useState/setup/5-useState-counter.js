c70f7b84ec759866733d00a7cc0175cbed07c664
tincurar <tincurares13@gmail.com>
2022-12-02 17:08:48 +0100

    finished chapter 5

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,38 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+
+  function complexIncrease() {
+    setTimeout(() => {
+      // setValue(value + 1)
+
+      // correct way. I need to update the previous State
+      setValue((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  }
+
+  return (
+    <>
+      <h2>useState counter example</h2>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>regulat counter</h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={() => setValue(value - 1)}>decrease</button>
+        <button className='btn' onClick={() => setValue(0)}>reset</button>
+        <button className='btn' onClick={() => setValue(value + 1)}>increase</button>
+      </section>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>more complex counter</h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={complexIncrease}>
+          increase later
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
